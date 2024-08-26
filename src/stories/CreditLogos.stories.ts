016f4806f5300b69fc42d62baf5459277d4290fd
Carifio24 <carifio24@gmail.com>
2024-08-26 15:05:47 -0400

    Add description to credit logo.

--- src/stories/CreditLogos.stories.ts
+++ src/stories/CreditLogos.stories.ts
@@ -14,7 +14,11 @@ type Story = StoryObj<typeof CreditLogos>;
 export const Primary: Story = {
   render: (args: CreditLogosProps) => ({
     components: { CreditLogos },
-    template: `<CreditLogos v-bind="args" />`,
+    template: `
+      <div style="width: fit-content">
+        <CreditLogos v-bind="args" />
+      </div>
+    `,
     setup() {
       return { args };
     }
@@ -29,5 +33,17 @@ export const Primary: Story = {
       }
     ],
     defaultLogos: ["wwt", "cosmicds", "nasa", "sciact"],
+    backgroundColor: "#3A3A3A",
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This component provides a convenient way to display logos of organizations affiliated with your project. \
+          Each logo can link to the organization's website. \
+          By default, this component displays logos associated with the CosmicDS project: WorldWide Telescope, CosmicDS, NASA, and NASA's Science Activation. \
+          However, you can use any subset (or none at all) of these logos logos via the `defaultLogos` prop. \
+          Additionally, you can add extra logos - see the example below with the Smithsonian logo for the required syntax."
+      }
+    }
   }
 };
